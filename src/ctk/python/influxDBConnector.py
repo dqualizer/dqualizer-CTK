2069242f65a6f3ad9e8b640e81a2c4fa934e93c2
Henning Möllers <henning.moellers@wps.de>
2023-12-28 14:37:16 +0100

    Update Influx Auth Data to InfluxDB in Dqualizer Container

--- src/ctk/python/influxDBConnector.py
+++ src/ctk/python/influxDBConnector.py
@@ -2,13 +2,14 @@ import influxdb_client, os, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-token = "PpJnMqWR0RkZGbd6GxocbTs9VyiHjFnOKpCiTVRaPYc3qySPKgS8oevYQwq0DNnnX7NZRlzcaAZ6bfcU5f1cuw==" # os.environ.get("INFLUXDB_TOKEN")
-org = "Dqualizer"
+
+token = "specialToken000"
+org = "my-org"
 url = "http://localhost:8086"
 
 write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 
-bucket = "ResilienceTesting"
+bucket = "my-bucket"
 
 write_api = write_client.write_api(write_options=SYNCHRONOUS)
 query_api = write_client.query_api()
@@ -20,4 +21,4 @@ def write_monitoring_data(measurement_id, tag_name, tag_value, field, value):
         .tag(tag_name, tag_value)
         .field(field, value)
     )
-    write_api.write(bucket=bucket, org="Dqualizer", record=point)
+    write_api.write(bucket=bucket, org=org, record=point)
