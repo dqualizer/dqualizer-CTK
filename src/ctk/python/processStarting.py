669e5d7357b0668ec228e44542639185cd2fc915
Henning Möllers <henning.moellers@wps.de>
2024-01-23 11:46:38 +0100

    Disable automatic process restarting.

--- src/ctk/python/processStarting.py
+++ src/ctk/python/processStarting.py
@@ -4,8 +4,8 @@ import subprocess
 # Alternativ mit import os, os.system(command) aber nicht empfohlen
 
 def start_process_by_path(path):
-    print(f"Start process: '{path}'")
-    subprocess.Popen([path], shell=True)
+    print(f"Please start process at {path} manually.")
+    #subprocess.Popen([path], shell=True)
 
 
 # Backslashes werden in Python escaped
