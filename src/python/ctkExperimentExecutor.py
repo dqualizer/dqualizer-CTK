4b6df78f13095b3424cce8a6d5dd8233120b6795
Henning Möllers <LeHenningo@users.noreply.github.com>
2024-02-13 09:06:24 +0100

    Adapted returned status codes.

--- src/python/ctkExperimentExecutor.py
+++ src/python/ctkExperimentExecutor.py
@@ -13,40 +13,37 @@ app = Flask(__name__)
 def run_experiment(chaos_experiment, result_queue):
     strategy: Strategy = Strategy.DEFAULT
     runner: run.Runner = run.Runner(strategy)
-    result = None
+    journal = {}
     try:
         # Journal can be written to file (json) and transformed to experiment report document (pdf)
         journal: Journal = runner.run(chaos_experiment)
-        # TODO TypeError: Object of type ValueError is not JSON serializable
         # Create file from journal. CAUTION: EXPOSES ENTERED SECRETS
         # with open(os.curdir, "w") as file:
         #     json.dump(journal, file)
         # logger.info(f"Experiment journal has been saved to {os.curdir}")
     except Exception as e:
         logger.error(e)
-        result_dict = {"exit_code": 500,
-                       "status": f"Experiment failed to execute.",
-                       "errorTrace": e}
+        result_dict = {"status_code": 500,
+                       "status": f"Experiment failed to execute and thrown exception was not caught by CTK.",
+                       "info": e}
         result_json = json.dumps(result_dict)
         result_queue.put(result_json)
 
     result_dict = {
-        "exit_code": 200,
-        "status": "Experiment was executed successfully.",
-        "errorTrace": ""
+        "status_code": 200 if journal["status"] == "completed" else 500,
+        "status": f"Experiment was executed and terminated with status: {journal["status"]}.",
+        "info": "See Python logs, (experiment journal) and dashboards for result infos"
     }
     result_json = json.dumps(result_dict)
     result_queue.put(result_json)
 
 
-# TODO Should be possible to ask for credentials here, so they not have to be sent by client --> NO TLS necessary for complete confidentiality!
-# TODO We can now modify secrets in given experiment
 @app.route('/execute_experiment', methods=['POST'])
 def execute_experiment():
     request_headers = request.headers
-    authentication_failed_json = jsonify({"exit_code": 401,
+    authentication_failed_json = jsonify({"status_code": 401,
                                           "status": f"Authentication failed.",
-                                          "errorTrace": ""})
+                                          "info": ""})
     try:
         authentication_connection = mySqlConnector.create_db_connection(request_headers.get('dbUsername'),
                                                                         request_headers.get('dbPassword'))
@@ -71,9 +68,9 @@ def execute_experiment():
             experiment.ensure_experiment_is_valid(chaos_experiment)
         except Exception as e:
             logger.error(e)
-            return jsonify({"exit_code": 400,
+            return jsonify({"status_code": 400,
                             "status": f"No valid experiment found.",
-                            "errorTrace": e})
+                            "info": e})
 
         result_queue = Queue()
 
