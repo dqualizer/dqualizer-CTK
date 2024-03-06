c000e85a213c82eac27c5091d4e3e602758e2109
Henning Möllers <LeHenningo@users.noreply.github.com>
2024-03-06 11:03:54 +0100

    processKilling.py checks for process name in jvm processes.

--- src/python/processKilling.py
+++ src/python/processKilling.py
@@ -1,19 +1,31 @@
+import subprocess
 import psutil
 import mySqlConnector
+import os
 
 
-# Kill process cross platform
+# Kill a process cross-platform
 def kill_process_by_name(db_username, db_password, username, password, process_name):
-
     connection = mySqlConnector.create_db_connection(db_username, db_password)
     authenticated = mySqlConnector.authenticate_user(connection, username, password)
 
     if authenticated:
+        # Get PID, in case searched processes runs in JVM, as they just have the name 'java' in os
+        pid_for_searched_jvm_process = None
+        os.environ['PATH'] = "C:\\Users\\HenningMöllers\\.jdks\\openjdk-20.0.1\\bin"
+        jps_processes = subprocess.check_output(["jps"]).decode("utf-8").split("\n")
+
+        for line in jps_processes:
+            if process_name in line:
+                line = line.split()
+                pid_for_searched_jvm_process = line[0]
+                break
+
         for proc in psutil.process_iter():
-            # Check whether the process name matches
-            if proc.name() == process_name:
+            # Check for process name or the eventually defined process id
+            if proc.name() == process_name or proc.pid == int(pid_for_searched_jvm_process):
                 print(f"Kill process: '{proc}'")
-                # do SIGKill to stop process gracefully
+                # Do SIGKill to stop process gracefully
                 proc.kill()
                 return True
 
@@ -23,5 +35,3 @@ def kill_process_by_name(db_username, db_password, username, password, process_n
     else:
         print(f"Failed to authenticate")
         return False
-
-
