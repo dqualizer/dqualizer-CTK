df6344184139d7e1430de3dfcd9e3dd66d747076
Henning Möllers <LeHenningo@users.noreply.github.com>
2024-04-10 15:04:58 +0200

    Added Todo.

--- src/python/processStarting.py
+++ src/python/processStarting.py
@@ -7,6 +7,6 @@ def start_process_by_path(db_username, db_password, username, password, path, lo
     print(f"Please start process at {path} manually.")
     #subprocess.Popen([path], shell=True)
 
-
+# TODO JAVA Process starten subprocess.Popen([java_command, "-jar", jar_file]
 # Backslashes werden in Python escaped
 # start_process_by_path(r"C:\Program Files\KeePassXC\KeePassXC.exe")
