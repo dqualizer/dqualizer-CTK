7e70798e63c9e1cb89c527ef1cc10376c9915767
Henning Möllers <LeHenningo@users.noreply.github.com>
2024-05-20 15:19:36 +0200

    Removed unused configuration field.

--- src/python/config.py
+++ src/python/config.py
@@ -1,6 +1,3 @@
-# generated_experiments_volume_path = "C:\\Users\\HenningMöllers\\IdeaProjects\\dqualizer\\docker-output\\generated_experiments"
-generated_experiments_volume_path = "C:\\Users\\HenningMöllers\\IdeaProjects\\dqexec\\generated_experiments"
-# has to be configured accordingly in dqExec
 experiment_executor_api_port = 3323
 db_base_config = {
     # Username and password are not declared here, see mySqlConnector.py
