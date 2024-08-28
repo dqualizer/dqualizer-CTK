dc0e04451e5d8a7f96d0a6d6c8844ed5ac7449e3
EddeCCC <eduard.schander@gmx.de>
2024-08-28 10:36:55 +0200

    write process_running measurement after restart

--- src/python/processStarting.py
+++ src/python/processStarting.py
@@ -2,14 +2,14 @@ import subprocess
 import os
 
 import mySqlConnector
-
+import influxDBConnector
 
 def start_process_by_path(db_username, db_password, username, password, path, log_result_in_influx_db):
     print(f"Please start process at {path} manually.")
     # subprocess.Popen([path], shell=True)
 
 
-def start_jvm_process_by_path(db_username, db_password, username, password, path):
+def start_jvm_process_by_path(db_username, db_password, username, password, path, process_name):
     connection = mySqlConnector.create_db_connection(db_username, db_password)
     authenticated = mySqlConnector.authenticate_user(connection, username, password)
 
@@ -19,6 +19,8 @@ def start_jvm_process_by_path(db_username, db_password, username, password, path
         os.environ['PATH'] = java_home + "\\bin"
         started_process_pid = subprocess.Popen(["java", "-jar", path]).pid
         print(f"Started process has PID: {started_process_pid}")
+        influxDBConnector.write_monitoring_data("process_running", "process_name", process_name, "is_running",
+                                                True)
 
     # TODO use Exception from chaostoolkit library if available
     else:
